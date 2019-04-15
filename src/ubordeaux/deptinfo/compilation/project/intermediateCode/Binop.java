1d5a5db18dc306b564af01801aa71fa573535c2c
Ambroise Coste <acoste@emi.u-bordeaux1.fr>
2019-04-15 18:42:47 +0200

    Debut de la generation de code #Binop

--- src/ubordeaux/deptinfo/compilation/project/intermediateCode/Binop.java
+++ src/ubordeaux/deptinfo/compilation/project/intermediateCode/Binop.java
@@ -12,7 +12,7 @@ public class Binop extends Exp {
 	}
 
 	public final static int PLUS = 0, MINUS = 1, MUL = 2, DIV = 3, AND = 4, OR = 5, LSHIFT = 6, RSHIFT = 7, ARSHIFT = 8,
-			XOR = 9;
+			XOR = 9, LESS = 10, LESSEQ = 11, MORE = 12, MOREEQ = 13 , EQ = 14, NOTEQ = 15, NOT = 16;
 
 	public String toString() {
 		String result = new String();
